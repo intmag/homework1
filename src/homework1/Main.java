181345ad6b3512c79198860c5aa0ec07b1d8e477
Evgeniy <e.kurynkin@gmail.com>
2020-10-07 12:10:58 +0300

    1-2 task

--- src/homework1/Main.java
+++ src/homework1/Main.java
@@ -1,4 +1,16 @@
 package homework1;
 
 public class Main {
+
+    public static void main(String[] args) {
+        byte byteVal = 127;
+        short shortVal = 32000;
+        int intVal = 64000;
+        long longVal = 128000000;
+        float floatVal = 12.1f;
+        double doubleVal = 12.1;
+        char charVal = 'X';
+        boolean booleanVal = true;
+
+    }
 }
